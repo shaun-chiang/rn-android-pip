525d2c589e61b84fd93045725377e3b4a2969b7b
yatan.vesh <yatan.vesh@gmail.com>
2020-07-07 04:16:37 +0530

    Upgrade buildToolsVersion->28.0.3, targetSdkVersion->28, downgrade minSdk->19, rename package name to reactpiplibrary to avoid name clash conflicts in release mode

---
+++ android/src/main/java/com/reactpiplibrary/RNAndroidPipModule.java
@@ -0,0 +1,27 @@
+
+package com.reactpiplibrary;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNAndroidPipModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNAndroidPipModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNAndroidPip";
+  }
+
+  @ReactMethod
+  public void enterPictureInPictureMode() {
+    getCurrentActivity().enterPictureInPictureMode();
+  }
+}
